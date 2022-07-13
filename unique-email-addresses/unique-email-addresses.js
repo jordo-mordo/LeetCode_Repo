457cfb37d924696526c78f896469c436ca58e5c1
Jordan Moore <jordanhmoore@gmail.com>
2022-07-13 11:36:56 -0400

    Time: 161 ms, Memory: 49.2 MB - LeetHub

---
+++ unique-email-addresses/unique-email-addresses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function(emails) {
+    let counter = 0;
+    let domains = {};
+    
+    emails.forEach((string, index) => {
+        let atSymbol = string.indexOf('@');
+        let domain = string.slice(atSymbol + 1, string.length);
+        let username = string.slice(0, atSymbol);
+        username = username.replaceAll('.', '');
+        if (username.includes('+')) {
+            username = username.slice(0, username.indexOf('+'));
+        }
+        
+        if (!domains[domain]) {
+            domains[domain] = [username];
+            counter++;
+        } else {
+            if (!domains[domain].includes(username)) {
+                domains[domain].push(username);
+                counter++;
+            }
+        }
+        
+    });
+    console.log(domains);
+    return counter;
+};
