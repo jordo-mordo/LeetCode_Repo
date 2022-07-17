f6def1e04e94064def4c38ccecd34db61d9ab597
Jordan Moore <jordanhmoore@gmail.com>
2022-07-17 14:28:10 -0400

    Time: 131 ms (56.51%), Space: 51.8 MB (15.49%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    let vals = {};
+    let result = false;
+    nums.forEach((val, index) => {
+        if (vals[val]) {
+            result = true;
+        } else {
+            vals[val] = true;
+        }
+    });
+    return result;
+};
