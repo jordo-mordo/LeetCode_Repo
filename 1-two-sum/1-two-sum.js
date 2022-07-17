dcf60ee4939e845e5b8cf6d1b12f41beaeffae6a
Jordan Moore <jordanhmoore@gmail.com>
2022-07-17 14:56:37 -0400

    Time: 62 ms (98.81%), Space: 42.8 MB (41.38%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,32 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    //input: array of nums
-    //output: array of two indeces that equal target
-    //constraints: number can not be added to itself, exactly one solution
-    //edge case: array of only two numbers
-    //__________________________________________________________
+    let map = {};
+    let value = 0;
     
-    //if array length is 2, return both indeces
-    if (nums.length === 2) {
-        return [0,1];
-    }
-    
-    //set up empty hash table
-    let numsObj = {};
-    //index through array
-    for(let i=0; i < nums.length; i++) {
-        const val = nums[i]
-        //store the target - current index
-        const diff = target - val;
-        //if the difference is included in hash table
-        if (diff in numsObj) {
-            //return indeces of both values
-            return [numsObj[diff], i]
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in map) {
+            return ([i, map[target - nums[i]]]);
+        } else {
+            map[nums[i]] = i;
         }
-        //add value to hash table
-        numsObj[val] = i;
     }
-    
-    return [];
 };
