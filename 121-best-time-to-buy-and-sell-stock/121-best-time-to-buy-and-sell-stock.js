5081c093a9cdb010becbd8c71b27415143e10268
Jordan Moore <jordanhmoore@gmail.com>
2022-07-20 15:16:04 -0400

    Time: 117 ms (59.28%), Space: 51.7 MB (53.62%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -7,15 +7,14 @@ var maxProfit = function(prices) {
     let buy = 0;
     let sell = 1;
     let maxProfit = 0;
-    
-    while (sell < prices.length) {
-        if (prices[buy] < prices[sell]) {
-            let profit = prices[sell] - prices[buy];
-            maxProfit = Math.max(maxProfit, profit);
-        } else {
-            buy = sell;
+                                                    //length is 6
+    while (sell < prices.length) {                    //sell index is 5
+        let profit = prices[sell] - prices[buy];     // profit is: 2
+        maxProfit = Math.max(maxProfit, profit);    // max profit is: 4
+        if (prices[sell] < prices[buy]) {           //
+            buy = sell;                             // buy -> 1
         }
-        sell += 1;
+        sell++;                                     // sell 6
     }
     
     return maxProfit;
